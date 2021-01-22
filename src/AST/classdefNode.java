4476d4065105e38bd09e256f5555ec30a647419c
Finnyear <finnyear@gmail.com>
2021-01-22 13:55:32 +0800

    冰冷苦海，无尽等待

--- src/AST/classdefNode.java
+++ src/AST/classdefNode.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 public class classdefNode extends PartNode{
     public String name;
-    public ArrayList<vardefNode> vardefs;
-    public ArrayList<fundefNode> fundefs;
-    public ArrayList<classconNode> classcons;
+    public ArrayList<vardefNode> vardefs = new ArrayList<>();
+    public ArrayList<fundefNode> fundefs = new ArrayList<>();
+    public ArrayList<classconNode> classcons = new ArrayList<>();
     public classdefNode(position pos, String name){
         super(pos);
         this.name = name;
